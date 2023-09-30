bb64693425ce1ebea76fa78d920601cf44ed4568
KimSound424 <ykj1512@gmail.com>
2023-09-30 23:43:22 +0900

    [Feature] 회원가입, 로그인 화면 구성

---
+++ src/pages/auth/SignIn.js
@@ -0,0 +1,14 @@
+const SignIn = () => {
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <form>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Sign In</button>
+        <button type="button">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+export default SignIn;
---
+++ src/pages/auth/SignUp.js
@@ -0,0 +1,15 @@
+const SignUp = () => {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <form>
+        <input type="text" placeholder="Name" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <input type="password" placeholder="Check Password" />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+export default SignUp;
